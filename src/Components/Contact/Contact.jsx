389f1353ef3817e06cf011f02938869336862346
Bacsó Ferenc <ferenc.bacso89@gmail.com>
2023-11-30 21:40:02 +0100

    change favico

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -18,16 +18,16 @@ const HandleSandbox=()=>{
   window.open("https://codesandbox.io/u/FerencBacso89","_blank")
 }
 const getCV =(event)=>{
-  if(event.target.id == "hunCV"){
+  if(event.target.id === "hunCV"){
     const link = document.createElement('a');
-    link.href = '../src/assets/CV/Bacsó-Ferenc_CV-HUN.pdf'; // Az elérési út a PDF fájlhoz
+    link.href = '../../assets/CV/Bacsó-Ferenc_CV-HUN.pdf'; // Az elérési út a PDF fájlhoz
     link.download = 'Bacsó-Ferenc_CV-HUN.pdf'; // A letöltött fájl neve
     link.target = '_blank';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
     }
-    if(event.target.id == "engCV"){
+    if(event.target.id === "engCV"){
       const link = document.createElement('a');
       link.href = '../src/assets/CV/Bacsó-Ferenc_CV-ENG.pdf'; // Az elérési út a PDF fájlhoz
       link.download = 'Bacsó-Ferenc_CV-ENG.pdf'; // A letöltött fájl neve
